f3ea7816137f81d188aa4a6db0af83a6e4c5bb3e
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-05-15 17:55:47 +0700

    step 11

--- src/components/404/Page404.jsx
+++ src/components/404/Page404.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import registrationImage from '../../../assets/image/registrationPage.jpg';
 
-function Page404() {
-  return <div>Not found</div>;
-}
+const Page404 = () => {
+  return (
+    <div className="text-center">
+      <img src={registrationImage} />
+      <h1 className="h4 text-muted">Страница не найдена</h1>
+      <p className="text-muted">Но вы можете перейти </p>
+      <Link to="/">на главную страницу</Link>
+    </div>
+  );
+};
 
 export default Page404;
